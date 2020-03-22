37d7d701406c424e10ad0f0cdb1a864bc42f1528
Alexandre Moro <alexandre.moro@outlook.com>
2020-03-22 01:42:26 +0100

    Factorisation en C

---
+++ S02/Ex7.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+
+#define N 5
+
+int multiply (int valeur, int coef ){
+    return valeur * coef;
+}
+
+// int power (int valeur, int exp) {
+
+//     double result = pow((double)valeur,(double)exp);
+//     return (int) result;
+
+// }
+
+int cap (int valeur, int cap) {
+    if (valeur > cap) {
+        return valeur = cap;
+    } else {
+        return valeur ;
+    }
+}
+
+void operation ( int inpuTable[], int outputTable[], int (*ptr)(), int option ){
+
+    
+    for (int i = 0; i < N; i++ ){
+        outputTable[i] = ptr(inpuTable[i], option);
+       
+    }
+
+}
+
+void main() {
+
+    int e[N] = {1,5,2,4,3};
+
+    int eDouble[N] = {};
+    operation(e,eDouble,multiply,2);
+
+    int etriple[N] = {};
+    operation(e,etriple,multiply,3);
+
+    // int ePow2[N] = {};
+    // operation(e,ePow2,power,2);
+
+    int eCap[N] = {};
+    operation(e,eCap,cap,3);
+
+
+
+}
