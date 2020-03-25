8534f0b597ee05234d9f2807be23f4eb83b95ab5
Alexandre Moro <alexandre.moro@outlook.com>
2020-03-25 23:42:00 +0100

    suite cours

--- S03/zoo.js
+++ S03/zoo.js
@@ -1,25 +1,27 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var ZooAnimal = /** @class */ (function () {
     // Constructeur
     function ZooAnimal(nom, age, poids) {
-        this.nom = nom;
         this.age = age;
         this.poids = poids;
         this.quantiteNourritureTotale = 0;
         this.bienNourri = false;
     }
-    // Méthodes
     ZooAnimal.prototype.nouvelleJournee = function () {
         this.quantiteNourritureTotale = 0;
         this.bienNourri = false;
-    };
-    ZooAnimal.prototype.recoitNourriture = function (quantite) {
-        this.quantiteNourritureTotale += quantite;
-        if (this.quantiteNourritureTotale > (this.poids / 5)) {
-            this.bienNourri = true;
-        }
-        else {
-            this.bienNourri = false;
-        }
     };
     ZooAnimal.prototype.etatAlimentation = function () {
         var etat = this.nom + ", " + this.age + " ans, a reçu " +
@@ -29,17 +31,57 @@ var ZooAnimal = /** @class */ (function () {
         }
         return etat;
     };
+    ZooAnimal.prototype.recoitNourriture = function (quantite) {
+        this.quantiteNourritureTotale += quantite;
+        this.bienNourri = this.calculBienNourri();
+    };
     return ZooAnimal;
 }());
-// Création d’instances de classes: objets
-var flipper = new ZooAnimal("Flipper", 30, 150);
-var pandi = new ZooAnimal("Pandi", 10, 80);
-// Appels de méthodes sur les objets
-flipper.nouvelleJournee();
-pandi.nouvelleJournee();
+var ZooDauphin = /** @class */ (function (_super) {
+    __extends(ZooDauphin, _super);
+    function ZooDauphin(name, age, poids, poissonPrefere) {
+        var _this = _super.call(this, name, age, poids) || this;
+        _this.poissonPrefere = poissonPrefere;
+        return _this;
+    }
+    ZooDauphin.prototype.etatAlimentation = function () {
+        return _super.prototype.etatAlimentation.call(this) + " Son poisson préféré est " + this.poissonPrefere + ".";
+    };
+    ZooDauphin.prototype.calculBienNourri = function () {
+        return this.quantiteNourritureTotale > this.poids / 8;
+    };
+    return ZooDauphin;
+}(ZooAnimal));
+var ZooPanda = /** @class */ (function (_super) {
+    __extends(ZooPanda, _super);
+    function ZooPanda(name, age, poids, plantePreferee) {
+        var _this = _super.call(this, name, age, poids) || this;
+        _this.plantePreferee = plantePreferee;
+        return _this;
+    }
+    ZooPanda.prototype.etatAlimentation = function () {
+        return _super.prototype.etatAlimentation.call(this) + " Sa plante préférée est " + this.plantePreferee + ".";
+    };
+    ZooPanda.prototype.calculBienNourri = function () {
+        return this.quantiteNourritureTotale > (this.poids / 4 - this.age / 10);
+    };
+    return ZooPanda;
+}(ZooAnimal));
+var flipper = new ZooDauphin("Flipper", 30, 150, "Fletan");
+var oum = new ZooDauphin("oum", 20, 100, "Sardine");
+var pandi = new ZooPanda("Pandi", 10, 80, "Bambou");
+var lesZanimaux = [flipper, oum, pandi];
+for (var _i = 0, lesZanimaux_1 = lesZanimaux; _i < lesZanimaux_1.length; _i++) {
+    var unAnimal = lesZanimaux_1[_i];
+    unAnimal.nouvelleJournee();
+}
 flipper.recoitNourriture(10);
 pandi.recoitNourriture(10);
 flipper.recoitNourriture(25);
-flipper.bienNourri = false; // <== casse l'intégrité de l'objet
-console.log(flipper.etatAlimentation());
-console.log(pandi.etatAlimentation());
+oum.recoitNourriture(15);
+for (var _a = 0, lesZanimaux_2 = lesZanimaux; _a < lesZanimaux_2.length; _a++) {
+    var unAnimal = lesZanimaux_2[_a];
+    console.log(unAnimal.etatAlimentation());
+}
+
+debugger;
--- S03/zoo.ts
+++ S03/zoo.ts
@@ -1,54 +1,95 @@
-class ZooAnimal {
+abstract class ZooAnimal {
 
     // Attributs
-    nom: string;
-    age: number;
-    poids: number;
-    quantiteNourritureTotale: number;
-    bienNourri: boolean;
+    protected nom: string;
+    protected age: number;
+    protected poids: number;
+    protected quantiteNourritureTotale: number;
+    private bienNourri: boolean;
 
     // Constructeur
     constructor(nom: string, age: number, poids: number) {
-        this.nom = nom;
         this.age = age;
         this.poids = poids;
         this.quantiteNourritureTotale = 0;
         this.bienNourri = false;
     }
 
-    // Méthodes
     nouvelleJournee() {
         this.quantiteNourritureTotale = 0;
         this.bienNourri = false;
     }
-    recoitNourriture(quantite: number) {
-        this.quantiteNourritureTotale += quantite;
-        if (this.quantiteNourritureTotale > (this.poids / 5)) {
-            this.bienNourri = true;
-        } else {
-            this.bienNourri = false;
-        }
-    }
+
     etatAlimentation() {
         let etat = this.nom + ", " + this.age + " ans, a reçu " +
-            this.quantiteNourritureTotale + "Kg de nourriture."
+            this.quantiteNourritureTotale + "Kg de nourriture.";
         if (this.bienNourri) {
             etat += " " + this.nom + " est bien nourri.";
         }
         return etat;
     }
+
+    protected abstract calculBienNourri()
+
+    recoitNourriture(quantite: number) {
+        this.quantiteNourritureTotale += quantite;
+        this.bienNourri = this.calculBienNourri();
+    }
+
+
 }
 
-// Création d’instances de classes: objets
-let flipper = new ZooAnimal("Flipper", 30, 150);
-let pandi = new ZooAnimal("Pandi", 10, 80);
+class ZooDauphin extends ZooAnimal {
 
-// Appels de méthodes sur les objets
-flipper.nouvelleJournee();
-pandi.nouvelleJournee();
+    private poissonPrefere: string;
+    constructor(name: string, age: number, poids: number, poissonPrefere: string) {
+        super(name, age, poids);
+        this.poissonPrefere = poissonPrefere;
+    }
+
+    etatAlimentation() {
+        return super.etatAlimentation() + " Son poisson préféré est " + this.poissonPrefere + ".";
+    }
+
+    protected calculBienNourri() {
+        return this.quantiteNourritureTotale > this.poids /8;
+    }
+
+}
+
+class ZooPanda extends ZooAnimal {
+
+    private plantePreferee: string;
+    constructor(name: string, age: number, poids: number, plantePreferee: string) {
+        super(name, age, poids);
+        this.plantePreferee = plantePreferee;
+    }
+
+    etatAlimentation() {
+        return super.etatAlimentation() + " Sa plante préférée est " + this.plantePreferee + ".";
+    }
+
+    protected calculBienNourri() {
+        return this.quantiteNourritureTotale > (this.poids / 4 - this.age / 10);
+    }
+
+}
+
+let flipper = new ZooDauphin("Flipper", 30, 150, "Fletan");
+let oum = new ZooDauphin("oum", 20, 100, "Sardine");
+let pandi = new ZooPanda("Pandi", 10, 80, "Bambou");
+
+let lesZanimaux:ZooAnimal[] = [flipper, oum, pandi];
+
+for(let unAnimal of lesZanimaux){
+    unAnimal.nouvelleJournee();
+}
+
 flipper.recoitNourriture(10);
 pandi.recoitNourriture(10);
 flipper.recoitNourriture(25);
-flipper.bienNourri = false;
-console.log(flipper.etatAlimentation());
-console.log(pandi.etatAlimentation());
+oum.recoitNourriture(15);
+
+for(let unAnimal of lesZanimaux) {
+    console.log(unAnimal.etatAlimentation());
+}
