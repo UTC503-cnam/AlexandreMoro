1d719f257e90b93a877f68f5b91d7acdd2a96f6f
Alexandre Moro <alexandre.moro@outlook.com>
2020-04-02 21:42:19 +0200

    Fibo

--- S05/fibonaccibase.js
+++ S05/fibonaccibase.js
@@ -1,10 +1,11 @@
 // Rappel : f(0) = 0, f(1) = 1, f(n) = f(n-1) + f(n-2)
-var n3 = 0;
+var n0 = 0;
+var n1 = 1;
+var n2 = 0;
 var test = 13;
-var result = 0;
-for (var i = 2; i < test; ++i) {
-    n3 = ((i - 2) + (i - 1));
-    result = result + n3;
-    console.log(i + ":" + result);
+for (var i = 2; i <= test; i++) {
+    n2 = n1 + n0;
+    n0 = n1;
+    n1 = n2;
 }
-console.log(result);
+console.log(n2);
--- S05/fibonaccibase.ts
+++ S05/fibonaccibase.ts
@@ -1,18 +1,17 @@
 // Rappel : f(0) = 0, f(1) = 1, f(n) = f(n-1) + f(n-2)
 
-let n0 = 0;
-let n1 = 1;
-let n3: number = 0;
+let n0: number = 0;
+let n1: number = 1;
+let n2: number = 0;
 let test: number = 13;
-let result: number = 0;
 
 
-for (let i: number = 2; i < test; ++i ) {
+for (let i: number = 2; i <= test; i++ ) {
 
-    n3 = ((i-2) + (i-1));
-    result = result + n3;
-
-    console.log(i + ":" + result);
+    n2 = n1 + n0;
+    n0 = n1;
+    n1 = n2;
 }
 
-console.log(result);
+console.log(n2);
+
