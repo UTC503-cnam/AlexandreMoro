8a89a0b57d4efc72d4e6adb8be803de115ce2815
Alexandre Moro <alexandre.moro@outlook.com>
2020-04-04 11:56:19 +0200

    Add ex webcfonf

---
+++ Webconf/if.js
@@ -0,0 +1,25 @@
+function fizzBuzz (number) {
+
+    if (number %3 == 0 && number %5 == 0) {
+      return "FizzBuzz";
+    
+    }
+    
+    if (number % 3 == 0) {
+      return "Fizz";
+    }
+    
+    if (number % 5 == 0) {
+      return "Buzz";
+    }
+    
+    
+    return number;
+    
+    }
+    
+    
+    console.log(fizzBuzz(15));
+    console.log(fizzBuzz(10));
+    console.log(fizzBuzz(9));
+    
