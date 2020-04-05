164dcd339ac5513fa4a69bb143e03a5339dda4f3
isamu1024 <alexandre.moro@outlook.com>
2020-04-05 13:17:43 +0200

    Restart because it was a mess

--- S05/fibonacci.ts
+++ S05/fibonacci.ts
@@ -1,137 +1,24 @@
-interface Iterator<T> {
+interface Iterateur<T>{
+    aUnSuivant(): boolean;
+    suivant(): T;
+}
 
-    current(): T;
+class Fibonacci {
+    // fibonacci(n) = fibonnacci(n-1) + fibonnacci(n-2)
 
-    next(): T;
 
-    key(): T;
 
-    valid(): boolean;
-
-    rewind(): void;
+if ()
 
 }
 
-interface Aggregator {
-
-    getIterator() : Iterator<number>;
-
+// Affiche les 10 premiers éléments de la suite de Fibonacci :
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34,
+// Fin (*)
+f = new Fibonacci(10); // (**)
+while(f.aUnSuivant()){
+    console.log(`${f.suivant()},`);
 }
 
-
-class fibonacciIterator implements Iterator<number> {
-
-    private collection: fiboCollec;
-
-    private position: number = 0;
-
-    private reverse: boolean = false;
-
-    constructor(collection: fiboCollec, reverse: boolean = false) {
-
-        this.collection = collection;
-        this.reverse = reverse;
-
-        if (reverse) {
-            this.position = collection.getCount() -1;
-        }
-
-    }
-
-    public rewind() {
-        this.position = this.reverse ? this.collection.getCount() - 1 : 0;
-    }
-
-    public current() : number {
-        return this.collection.getItems()[this.position];
-    }
-
-    public key(): number {
-        return this.position;
-    }
-
-    public next() : any {   // si je mets number, j'ai une erreur mais ça compile (???????), any > plus d'erreur et aucune idée pourquoi :/
-        const item = this.collection.getItems()[this.position];
-        this.position += this.reverse ? -1 : 1;
-        return item;
-    }
-    
-    public valid(): boolean {
-        if (this.reverse) {
-            return this.position >= 0;  
-        }
-
-        return this.position < this.collection.getCount();
-    }
-
-}
-
-class fiboCollec implements Aggregator{
-
-    constructor(fNumber: number) {
-        this.fibAlgoToCollec(fNumber);
-    }
-
-    private items: number[] = [];
-
-    public getItems(): number[] {
-        return this.items;
-    }
-
-    public getCount(): number {
-        return this.items.length;
-    }
-
-    public addItem(item: number) : void {
-        this.items.push(item);
-    }
-
-    public getIterator(): Iterator<number> {
-        return new fibonacciIterator(this);
-    }
-
-    public getreverseIterator(): Iterator<number> {
-        return new fibonacciIterator(this, true);
-    }
-
-    private fibAlgoToCollec(fnumber: number): void {
-
-        let number0: number = 0;
-        let number1: number = 1;
-        let number2: number = 0;
-
-        for (let i: number = 2; i <= fnumber; i++ ) {
-
-            number2 = number1 + number0;
-            number0 = number1;
-            number1 = number2;
-        
-            this.addItem(number2);
-        }
-
-    }
-
-}
-
-
-const collection = new fiboCollec(20);
-
-//const subject = new ConcreteSubject();
-
-const iterator = collection.getIterator();
-
-console.log("Normal");
-
-while (iterator.valid()) {
-    console.log(iterator.next());
-}
-
-console.log("-----");
-console.log("Reverse");
-
-const reverseIterator = collection.getreverseIterator();
-
-while (reverseIterator.valid()) {
-    console.log(reverseIterator.next());
-}
-
+// (*)  Ex2 : l'observeur affiche 'Fin' à la fin de la série
+// (**) Ex3 : remplacer ce code par une initialisation par Singleton
