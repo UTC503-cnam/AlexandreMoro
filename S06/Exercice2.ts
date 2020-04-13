24b134daa2d84c4d03db7ee0c86647850f4cd866
isamu1024 <alexandre.moro@outlook.com>
2020-04-13 11:50:55 +0200

    Ex2

--- S06/Exercice2.ts
+++ S06/Exercice2.ts
@@ -1,5 +1,16 @@
 //Réécrivez l’exercice 6 du chapitre 2 sur l’impératif
-//à l’aide de cettefoncton et de foncton lambda
+//à l’aide de cette foncton et de foncton lambda
+
+let double = (x: number) => x * 2;
+let triple = (x: number) => x * 3;
+let square = (x: number) => x * x;
+let maxi3 = (x: number) => (x > 3 ? (x = 3) : x);
 
 let tab = [1, 5, 2, 4, 3];
-tab.map(my_function);
+
+let tabDouble = tab.map(double);
+let tabTriple = tab.map(triple);
+let tabSquare = tab.map(square);
+let tabmax3 = tab.map(maxi3);
+
+debugger;
