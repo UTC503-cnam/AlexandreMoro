ea86def6aea56e8058c27be4c8b63a481b8d429d
Alexandre Moro <alexandre.moro@outlook.com>
2020-04-21 15:35:29 +0200

    fix

--- S07/ex1.ts
+++ S07/ex1.ts
@@ -10,13 +10,10 @@ function curry_format_message(frome) {}
 */
 
 let format_message = (from: string) => (to: string) => (message: string) => {
-  (`From: ${from} | to ${to}: ${message}`)};
+  return `From: ${from} | to ${to}: ${message}`;
 };
-
 let message = format_message("Liam")("Bad guys")(
   "I will look for you, I will find you and I will kill you."
 );
 
 console.log(message);
-
-
