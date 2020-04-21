f892fedfb1c993d847c3080603f3e697fe665ca8
Alexandre Moro <alexandre.moro@outlook.com>
2020-04-21 15:34:28 +0200

    Delete console.log() dans la fonction

--- S07/ex1.ts
+++ S07/ex1.ts
@@ -10,15 +10,13 @@ function curry_format_message(frome) {}
 */
 
 let format_message = (from: string) => (to: string) => (message: string) => {
-  console.log(`From: ${from} | to ${to}: ${message}`);
+  (`From: ${from} | to ${to}: ${message}`)};
 };
 
 let message = format_message("Liam")("Bad guys")(
   "I will look for you, I will find you and I will kill you."
 );
 
-let incompleteMessage = format_message("Alex");
-let incompleteMessage_2 = format_message("Alex")("Enfants");
-let incompleteMessage_3 = format_message("Alex")("Enfants")(
-  "Rangez vos chambres !!!"
-);
+console.log(message);
+
+
