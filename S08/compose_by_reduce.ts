93df266a1f00b306e1de64de88172a01c5155217
isamu1024 <alexandre.moro@outlook.com>
2020-04-28 22:38:57 +0200

    wip exercice

---
+++ S08/compose_by_reduce.ts
@@ -0,0 +1,10 @@
+
+let square = x => x * x;
+let double = x => x * 2;
+let half = x => x / 2;
+let square_root = x => Math.sqrt(x);
+
+// Ecrivez la fonction lambda compose à l'aide de la fonction reduceRight
+let compose = 
+
+console.log('sqrt((10*10) * 2 / 2) = ' + compose(square_root, half, double, square)(10));
