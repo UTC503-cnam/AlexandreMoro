5f22f8b032efa81c409d85e6aa5d85da42e9595d
Alexandre Moro <alexandre.moro@outlook.com>
2020-04-29 23:21:39 +0200

    ajout ex

---
+++ S08/compose_by_lodash.ts
@@ -0,0 +1,16 @@
+// A faire avant :
+// npm i --save lodash  --save: Package will appear in your dependencies
+// npm i @types/node   TypeScript definitions for Node.js (???)
+
+var fp = require("lodash/fp");
+
+let squared = (x) => x * x;
+let doubled = (x) => x * 2;
+let halfed = (x) => x / 2;
+let squared_root = (x) => Math.sqrt(x);
+
+// Utilisez une fonction de lodash pour composer
+console.log(
+  "sqrt((10*10) * 2 / 2) = " +
+    fp.compose(squared_root, halfed, doubled, squared, 10)
+);
