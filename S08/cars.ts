4ac0ae520a8eb3a1e6782b24f9d364dc7c865b7e
Alexandre Moro <alexandre.moro@outlook.com>
2020-05-02 18:41:00 +0200

    wip

--- S08/cars.ts
+++ S08/cars.ts
@@ -1,11 +1,3 @@
-// Convertissez les fonctions de cars.ts en fonction lambda, en utilisant au besoin filter et reduce
-
-/**
- *
- *     ERREUR : A REVOIR
- *
- */
-
 class Car {
   constructor(
     public brand: string,
@@ -14,33 +6,19 @@ class Car {
   ) {}
 }
 
-function maxPrice(cars: Car[]): number {
-  return cars.reduce((a, b) => {
-    return a.price < b.price ? b : a;
-  }).price;
-}
+let maxPrice: Function = (x: Car[]) =>
+  x.reduce(
+    (currentMax, car) => (car.price > currentMax ? car.price : currentMax),
+    0
+  );
 
-function minPrice(cars: Car[]): number {
-  return cars.reduce((a, b) => {
-    return a.price < b.price ? a : b;
-  }).price;
-}
+let minPrice: Function = (x: Car[]) =>
+  x.reduce(
+    (currentMin, car) => (car.price < currentMin ? car.price : currentMin),
+    Number.MAX_VALUE
+  );
 
-function averagePrice(cars: Car[]): number {
-  return cars.map((a) => a.price).reduce((a, b) => a + b) / cars.length;
-}
-
-// function averagePriceByBrand(cars: Car[], brand: string): number {
-//   return (
-//     cars
-//       .filter((a) => a.brand === brand)
-//       .map((a) => a.price)
-//       .reduce((a, b) => a + b) / cars.filter((a) => a.brand === brand).length
-//   );
-// }
-
-let averagePriceByBrand = (cars: Car[]) => (brand: string) =>
-  averagePrice(cars.filter((car) => car.brand === brand));
+let averagePrice : Function = (x: Car[]) =>
 
 let cars: Car[] = [
   new Car("Renault", "Twizy", 7540),
@@ -54,45 +32,4 @@ let cars: Car[] = [
 ];
 
 console.log("Max price: " + maxPrice(cars));
-console.log("Min price: " + minPrice(cars));
-console.log("Average price: " + averagePrice(cars));
-console.log(
-  "Average price for Porsche: " + averagePriceByBrand(cars, "Porsche")
-);
-
-/**
- * 
- * class Car {
-    constructor(public brand: string,
-        public model: string,
-        public price: number){
-
-        }
-}
-
-let maxPrice = cars => cars.reduce((maxPrice, car) => car.price > maxPrice ? car.price : maxPrice, 0);
-
-let minPrice = cars => cars.reduce((minPrice, car) => car.price < minPrice ? car.price : minPrice, Number.MAX_VALUE);
-
-let averagePrice = cars => cars.reduce((total, car) => total + car.price, 0) / cars.length;
-
-let averagePriceByBrand = cars => brand => averagePrice(cars.filter(car => car.brand === brand));
-
-let cars: Car[] = [new Car('Renault', 'Twizy', 7540), 
-                   new Car('Alfa Romeo', 'Mito', 15490),
-                   new Car('Volkswagen', 'Golf', 21990),
-                   new Car('Porsche', '718', 55040),
-                   new Car('BMW', 'Serie 1', 23200),
-                   new Car('Audi', 'A4', 33670),
-                   new Car('Porsche', 'Macan', 58835),
-                   new Car('Volkswagen', 'Polo', 14430),
-];
-
-console.log('Max price: ' + maxPrice(cars));
-console.log('Min price: ' + minPrice(cars));
-console.log('Average price: ' + averagePrice(cars));
-console.log('Average price for Porsche: ' + averagePriceByBrand(cars)('Porsche'));
-
- * 
- * 
- */
+debugger;
