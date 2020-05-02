a5545d525ef950aca3270b6253e20ce7e1123fe6
Alexandre Moro <alexandre.moro@outlook.com>
2020-05-02 22:14:28 +0200

    test promise sur car.ts

--- S08/cars.ts
+++ S08/cars.ts
@@ -1,35 +1,45 @@
 class Car {
-  constructor(
-    public brand: string,
-    public model: string,
-    public price: number
-  ) {}
+	constructor(
+		public brand: string,
+		public model: string,
+		public price: number
+	) {}
 }
 
-let maxPrice: Function = (x: Car[]) =>
-  x.reduce(
-    (currentMax, car) => (car.price > currentMax ? car.price : currentMax),
-    0
-  );
+let display = (result: any) => {
+	console.log(`${result}`);
+};
+
+let maxPrice = function (x: Car[]) {
+	return new Promise((resolve, reject) => {
+		resolve(
+			x.reduce(
+				(currentMax, car) => (car.price > currentMax ? car.price : currentMax),
+				0
+			)
+		);
+		reject(`une erreur est survenue`);
+	});
+};
 
 let minPrice: Function = (x: Car[]) =>
-  x.reduce(
-    (currentMin, car) => (car.price < currentMin ? car.price : currentMin),
-    Number.MAX_VALUE
-  );
+	x.reduce(
+		(currentMin, car) => (car.price < currentMin ? car.price : currentMin),
+		Number.MAX_VALUE
+	);
 
-let averagePrice : Function = (x: Car[]) =>
+let averagePrice: Function = (x: Car[]) =>
+	x.reduce((a, b) => a + b.price, 0) / x.length;
 
 let cars: Car[] = [
-  new Car("Renault", "Twizy", 7540),
-  new Car("Alfa Romeo", "Mito", 15490),
-  new Car("Volkswagen", "Golf", 21990),
-  new Car("Porsche", "718", 55040),
-  new Car("BMW", "Serie 1", 23200),
-  new Car("Audi", "A4", 33670),
-  new Car("Porsche", "Macan", 58835),
-  new Car("Volkswagen", "Polo", 14430),
+	new Car("Renault", "Twizy", 7540),
+	new Car("Alfa Romeo", "Mito", 15490),
+	new Car("Volkswagen", "Golf", 21990),
+	new Car("Porsche", "718", 55040),
+	new Car("BMW", "Serie 1", 23200),
+	new Car("Audi", "A4", 33670),
+	new Car("Porsche", "Macan", 58835),
+	new Car("Volkswagen", "Polo", 14430),
 ];
 
-console.log("Max price: " + maxPrice(cars));
-debugger;
+maxPrice(cars).then(display);
