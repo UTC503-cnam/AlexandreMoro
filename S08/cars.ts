4642a8475222efb56c8c13d7ddded5506c470f71
isamu1024 <alexandre.moro@outlook.com>
2020-05-03 11:41:55 +0200

    testing promise on car

--- S08/cars.ts
+++ S08/cars.ts
@@ -1,58 +1,56 @@
 class Car {
-	constructor(
-		public brand: string,
-		public model: string,
-		public price: number
-	) {}
+  constructor(
+    public brand: string,
+    public model: string,
+    public price: number
+  ) {}
 }
 
-let display = (result: any) => {
-	console.log(`${result}`);
+let displayMaxi = (result: any) => {
+  console.log(`Le prix maximum est de de: ${result}`);
+};
+
+let displayMini = (result: any) => {
+  console.log(`Le prix minimum est de: ${result}`);
 };
 
 let maxPrice = function (x: Car[]) {
-	return new Promise((resolve, reject) => {
-		resolve(
-			x.reduce(
-				(currentMax, car) => (car.price > currentMax ? car.price : currentMax),
-				0
-			)
-		);
-		reject(`une erreur est survenue`);
-	});
+  return new Promise((resolve, reject) => {
+    resolve(
+      x.reduce(
+        (currentMax, car) => (car.price > currentMax ? car.price : currentMax),
+        0
+      )
+    );
+    reject(`une erreur est survenue`);
+  });
 };
 
 let minPrice = function (x: Car[]) {
-	return new Promise((resolve, reject) => {
-		resolve(
-			x.reduce(
-				(currentMin, car) => (car.price < currentMin ? car.price : currentMin),
-				Number.MAX_VALUE
-			)
-		);
-		reject(`une erreur est surevenue`);
-	});
+  return new Promise((resolve, reject) => {
+    resolve(
+      x.reduce(
+        (currentMin, car) => (car.price < currentMin ? car.price : currentMin),
+        Number.MAX_VALUE
+      )
+    );
+    reject(`une erreur est surevenue`);
+  });
 };
 
-// let minPrice: Function = (x: Car[]) =>
-// 	x.reduce(
-// 		(currentMin, car) => (car.price < currentMin ? car.price : currentMin),
-// 		Number.MAX_VALUE
-// 	);
-
 let averagePrice: Function = (x: Car[]) =>
-	x.reduce((a, b) => a + b.price, 0) / x.length;
+  x.reduce((a, b) => a + b.price, 0) / x.length;
 
 let cars: Car[] = [
-	new Car("Renault", "Twizy", 7540),
-	new Car("Alfa Romeo", "Mito", 15490),
-	new Car("Volkswagen", "Golf", 21990),
-	new Car("Porsche", "718", 55040),
-	new Car("BMW", "Serie 1", 23200),
-	new Car("Audi", "A4", 33670),
-	new Car("Porsche", "Macan", 58835),
-	new Car("Volkswagen", "Polo", 14430),
+  new Car("Renault", "Twizy", 7540),
+  new Car("Alfa Romeo", "Mito", 15490),
+  new Car("Volkswagen", "Golf", 21990),
+  new Car("Porsche", "718", 55040),
+  new Car("BMW", "Serie 1", 23200),
+  new Car("Audi", "A4", 33670),
+  new Car("Porsche", "Macan", 58835),
+  new Car("Volkswagen", "Polo", 14430),
 ];
 
-maxPrice(cars).then(display);
-minPrice(cars).then(display);
+maxPrice(cars).then(displayMaxi);
+minPrice(cars).then(displayMini);
