db1d414c349de6446a2287fc8d179ef0f2638717
isamu1024 <alexandre.moro@outlook.com>
2020-05-16 19:09:37 +0200

    fix indent

--- Webconf/FizzBuzz.js
+++ Webconf/FizzBuzz.js
@@ -1,25 +1,19 @@
-function fizzBuzz (number) {
+function fizzBuzz(number) {
+  if (number % 3 == 0 && number % 5 == 0) {
+    return "FizzBuzz";
+  }
 
-    if (number %3 == 0 && number %5 == 0) {
-      return "FizzBuzz";
-    
-    }
-    
-    if (number % 3 == 0) {
-      return "Fizz";
-    }
-    
-    if (number % 5 == 0) {
-      return "Buzz";
-    }
-    
-    
-    return number;
-    
-    }
-    
-    
-    console.log(fizzBuzz(15));
-    console.log(fizzBuzz(10));
-    console.log(fizzBuzz(9));
-    
+  if (number % 3 == 0) {
+    return "Fizz";
+  }
+
+  if (number % 5 == 0) {
+    return "Buzz";
+  }
+
+  return number;
+}
+
+console.log(fizzBuzz(15));
+console.log(fizzBuzz(10));
+console.log(fizzBuzz(9));
