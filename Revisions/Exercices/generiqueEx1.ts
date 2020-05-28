2ac8399ef03dedba8c81607ea2a05a21aad61450
Alexandre Moro <alexandre.moro@outlook.com>
2020-05-28 09:47:31 +0200

    ex

--- Revisions/Exercices/generiqueEx1.ts
+++ Revisions/Exercices/generiqueEx1.ts
@@ -1,50 +1,63 @@
+/*
+Reprise de l'ex1 générique avec une petite pointe de PF dedans.
+*/
+
 export interface Mesurable {
-  length: number;
+	length: number;
 }
 
 export class Trajet implements Mesurable {
-  private nom: string;
-  length: number;
+	private nom: string;
+	length: number;
 
-  constructor(nom: string, length: number) {
-    this.nom = nom;
-    this.length = length;
-  }
+	constructor(nom: string, length: number) {
+		this.nom = nom;
+		this.length = length;
+	}
 
-  toString(): string {
-    return `Trajet ${this.nom} de ${this.length}Km`;
-  }
+	toString(): string {
+		return `Trajet ${this.nom} de ${this.length}Km`;
+	}
 }
 
-export class MinMax {
-  constructor(t: Array) {
-    this.computeMinMax(t);
-  }
+export class MinMax<T extends Mesurable> {
+	tab: Array<T>;
 
-  private computeMinMax(t: Array) {
-    for (let e of t) {
-    }
-  }
+	constructor(tab: Array<T>) {
+		this.tab = tab;
+	}
 
-  getMin(): {};
+	public getMin(): T {
+		return this.tab.reduce((prev, curr) =>
+			curr.length < prev.length ? curr : prev
+		);
+	}
 
-  getMax() {}
+	public getMax(): T {
+		return this.tab.reduce((prev, curr) =>
+			curr.length > prev.length ? curr : prev
+		);
+	}
 }
 
-let trajets: Array = new Array(
-  new Trajet("a", 271),
-  new Trajet("b", 161),
-  new Trajet("c", 314)
+let trajets: Array<Trajet> = new Array(
+	new Trajet("a", 271),
+	new Trajet("b", 161),
+	new Trajet("c", 314)
 );
-let trajetMinMax = console.log(
-  `Le plus grand trajet est : ${trajetMinMax.getMax()}`
-);
+let trajetMinMax = new MinMax(trajets);
+
+console.log(`Le plus grand trajet est : ${trajetMinMax.getMax()}`);
 console.log(`Le plus petit trajet est : ${trajetMinMax.getMin()}`);
 
-let chaines: Array = new Array("chaineDe9", "chDe5", "grandeChaineDe16");
-let chaineMinMax = console.log(
-  `La plus grande chaine est : ${chaineMinMax.getMax()}`
+let chaines: Array<String> = new Array(
+	"chaineDe9",
+	"chDe5",
+	"grandeChaineDe16"
 );
+let chaineMinMax = new MinMax(chaines);
+
+console.log(`La plus grande chaine est : ${chaineMinMax.getMax()}`);
 console.log(`La plus petite chaine est : ${chaineMinMax.getMin()}`);
 
 // Replace console.log by alert when run on TypeScript Playground
